e20e2516068303218d9c1e9e98bed243dc5c2edd
nisseknudsen <nisse.knudsen@gmail.com>
2024-11-08 10:48:24 -0800

    Upgraded to latest PD SDK version

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ from typing import Tuple
 
 from make87 import (
     initialize,
-    get_subscriber_topic,
-    get_publisher_topic,
+    get_subscriber,
+    get_publisher,
     resolve_topic_name,
-    MultiSubscriberTopic,
+    MultiSubscriber,
 )
 from make87_messages.geometry.box.box_2d_pb2 import Box2DAxisAligned
 from make87_messages.image.compressed.image_jpeg_pb2 import ImageJPEG
@@ -16,7 +16,7 @@ import numpy as np
 def main():
     initialize()
     topic_name = resolve_topic_name("CROPPED_IMAGE")
-    publisher_topic = get_publisher_topic(name=topic_name, message_type=ImageJPEG)
+    publisher_topic = get_publisher(name=topic_name, message_type=ImageJPEG)
 
     def crop_and_publish_image(messages: Tuple[Box2DAxisAligned, ImageJPEG]):
         bounding_box_msg, image_msg = messages
@@ -42,10 +42,10 @@ def main():
             publisher_topic.publish(cropped_image_message)
             print("Published cropped image")
 
-    bounding_box_topic = get_subscriber_topic(name=resolve_topic_name("BOUNDING_BOX_2D"), message_type=Box2DAxisAligned)
-    image_topic = get_subscriber_topic(name=resolve_topic_name("IMAGE_DATA"), message_type=ImageJPEG)
+    bounding_box_topic = get_subscriber(name=resolve_topic_name("BOUNDING_BOX_2D"), message_type=Box2DAxisAligned)
+    image_topic = get_subscriber(name=resolve_topic_name("IMAGE_DATA"), message_type=ImageJPEG)
 
-    multi_subscriber = MultiSubscriberTopic(delta_time=0.1)
+    multi_subscriber = MultiSubscriber(delta_time=0.1)
     multi_subscriber.add_topic(bounding_box_topic)
     multi_subscriber.add_topic(image_topic)
     multi_subscriber.subscribe(crop_and_publish_image)
